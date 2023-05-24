543698dda46ca526791a79ac41848ba3e33daf11
Zorag44 <75122924+Zorag44@users.noreply.github.com>
2023-05-25 02:04:54 +0530

    Update twilio.service.ts

--- src/app/twilio.service.ts
+++ src/app/twilio.service.ts
@@ -4,10 +4,10 @@ import axios from 'axios';
   providedIn: 'root'
 })
 export class TwilioService {
-  private apiUrl = 'https://api.twilio.com/2010-04-01/Accounts/ACc3c8933cf8b6b1d173d66fc667876d67/Messages.json';
-  private accountSid = 'ACc3c8933cf8b6b1d173d66fc667876d67';
-  private authToken = '496d5a7a8c94ce7d911700869b5d6d0f';
-  private fromPhoneNumber = '+12545405639';
+  private apiUrl = 'https://api.twilio.com/2010-04-01/Accounts//Messages.json';
+  private accountSid = '';
+  private authToken = '';
+  private fromPhoneNumber = '';
   constructor() { }
   sendSMS(toPhoneNumber:String, message:String):Promise<any>{
     const payload={
